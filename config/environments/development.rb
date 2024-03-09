105d68c9ac8a212e6915666599772e4c902b1298
tomo1070 <ec2-user@ip-172-31-20-240.ap-northeast-1.compute.internal>
2024-03-09 01:48:05 +0000

    [Finish] 3-1

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,6 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  
+  config.hosts << "acf99326cebf455d8426f513bf2daa1e.vfs.cloud9.ap-northeast-1.amazonaws.com"
 end
